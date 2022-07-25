36c8f1d740e4dac576978ea9e651560cb3a0c4c6
Kevin Tran <kevintran1142@gmail.com>
2022-07-24 23:45:43 -0700

    note about the request object

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -8,6 +8,7 @@ class SignupController extends Controller
 {
     //
     public function index(Request $request){
+        // using request to grab email specifically
        print_r($request->input("email"));
        return view("signup");
     }
