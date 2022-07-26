83afcf3143715afc20c92184e22bb3b427f10437
Kevin Tran <kevintran1142@gmail.com>
2022-07-25 21:51:44 -0700

    adding validation

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -10,6 +10,8 @@ class SignupController extends Controller
     public function index(Request $request){
         // using request to grab email specifically
        print_r($request->input("email"));
+       $array['email'] = "required";
+       $validate = $request->validate($array);
        return view("signup");
     }
 }
