bba8a32d627f3a9e8efaaed7c1678a337a42935a
Kevin Tran <kevintran1142@gmail.com>
2022-07-25 22:23:38 -0700

    limiting password to at least 4 characters

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -6,12 +6,17 @@ use Illuminate\Http\Request;
 
 class SignupController extends Controller
 {
-    //
+    public function get() {
+        return view('signup');
+    }
+
     public function index(Request $request){
         // using request to grab email specifically
-       print_r($request->input("email"));
-       $array['email'] = "required";
+       $array['email'] = "required|email";
+       $array['password'] = "required|min:4";
+
        $validate = $request->validate($array);
+       print_r($validate);
        return view("signup");
     }
 }
